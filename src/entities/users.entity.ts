817a29a7e639e28c989546c8650b10bc8c2db600
aljubaer <bsse0812@iit.du.ac.bd>
2022-05-06 22:02:46 +0600

    setup initial project

---
+++ src/entities/users.entity.ts
@@ -0,0 +1,30 @@
+import { IsNotEmpty } from 'class-validator';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { User } from '@interfaces/users.interface';
+
+@Entity()
+export class UserEntity extends BaseEntity implements User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @IsNotEmpty()
+  @Unique(['email'])
+  email: string;
+
+  @Column()
+  @IsNotEmpty()
+  password: string;
+
+  @Column()
+  @IsNotEmpty()
+  role: 'admin' | 'customer';
+
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column()
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
